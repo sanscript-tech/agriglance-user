a02a300b3ec4f74dc289fe1594de23aaf293823c
Manthan Gupta <manthangupta109@gmail.com>
2020-11-24 10:26:34 +0530

    Home Screen

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:agriglance/Screens/materials_home.dart';
+import 'package:agriglance/Screens/test_home.dart';
 import 'package:agriglance/Services/authentication_service.dart';
 import 'package:agriglance/Services/firestore_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -15,26 +17,55 @@ class Home extends StatefulWidget {
   }
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController _tabController;
   @override
+  void initState() {
+    _tabController = new TabController(length: 4, vsync: this);
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return StreamProvider<QuerySnapshot>.value(
       value: FirestoreService().getReference.snapshots(),
       child: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Agriglance")),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.logout),
-              onPressed: () {
-                context.read<AuthenticationService>().signOut();
-              },
+          appBar: AppBar(
+            title: Center(child: Text("Agriglance")),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () {
+                  context.read<AuthenticationService>().signOut();
+                },
+              ),
+            ],
+            bottom: TabBar(
+              controller: _tabController,
+              onTap: (index) {},
+              tabs: [
+                Tab(
+                  text: "Test",
+                ),
+                Tab(
+                  text: "Material",
+                ),
+                Tab(text: "QNA"),
+                Tab(
+                  text: "Jobs",
+                )
+              ],
             ),
-          ],
-        ),
-        body: Center(
-            child: Text("Welcome ${FirebaseAuth.instance.currentUser.email}")),
-      ),
+          ),
+          drawer: Drawer(),
+          body: TabBarView(
+            controller: _tabController,
+            children: [
+              TestHome(),
+              MaterialsHome(),
+              Text("This is notification Tab View"),
+              Text("This is notification Tab View"),
+            ],
+          )),
     );
   }
 }
