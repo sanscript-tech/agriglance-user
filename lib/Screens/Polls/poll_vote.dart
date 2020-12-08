7aea70248670b13a852739eb2e44562e64d92541
Manthan Gupta <manthangupta109@gmail.com>
2020-12-08 23:15:53 +0530

    State Management added to poll vote screen

--- lib/Screens/Polls/poll_vote.dart
+++ lib/Screens/Polls/poll_vote.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 
 class PollVote extends StatefulWidget {
+  final String question;
+  final String option1;
+  final String option2;
+  final String option3;
+  final String option4;
+  final int totalVotesOnOption1;
+  final int totalVotesOnOption2;
+  final int totalVotesOnOption3;
+  final int totalVotesOnOption4;
+  final int index;
+  PollVote(
+      {this.option1,
+      this.option2,
+      this.option3,
+      this.option4,
+      this.question,
+      this.totalVotesOnOption1,
+      this.totalVotesOnOption2,
+      this.totalVotesOnOption3,
+      this.totalVotesOnOption4,
+      this.index});
   @override
   _PollVoteState createState() => _PollVoteState();
 }
@@ -13,7 +34,9 @@ class _PollVoteState extends State<PollVote> {
         title: Text("Vote"),
         centerTitle: true,
       ),
-      
+      body: Column(
+        children: [],
+      ),
     );
   }
 }
