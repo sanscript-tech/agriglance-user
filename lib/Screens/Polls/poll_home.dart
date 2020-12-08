77bde6e1bab472b8588929af361a4b40a9a89ad4
Manthan Gupta <manthangupta109@gmail.com>
2020-12-08 22:44:58 +0530

    Poll card initialised

--- lib/Screens/Polls/poll_home.dart
+++ lib/Screens/Polls/poll_home.dart
@@ -1,4 +1,6 @@
 import 'package:agriglance/Screens/Polls/add_polls.dart';
+import 'package:agriglance/constants/poll_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class PollHome extends StatefulWidget {
@@ -13,6 +15,21 @@ class _PollHomeState extends State<PollHome> {
       appBar: AppBar(
         title: Text("Polls"),
         centerTitle: true,
+      ),
+      body: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("polls").snapshots(),
+          builder: (context, snapshot) {
+            return !snapshot.hasData
+                ? Text("Loading")
+                : ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index) {
+                      return PollCard();
+                    },
+                  );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
---
+++ lib/constants/poll_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PollCard extends StatefulWidget {
+  @override
+  _PollCardState createState() => _PollCardState();
+}
+
+class _PollCardState extends State<PollCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
