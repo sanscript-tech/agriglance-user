97ad44e81d15e6eb96c9d42a7551ddde14ecc570
Manthan Gupta <manthangupta109@gmail.com>
2020-12-10 16:07:43 +0530

    Submit video screen styled

--- lib/Screens/Videos/submit_video.dart
+++ lib/Screens/Videos/submit_video.dart
@@ -22,85 +22,87 @@ class _SubmitVideoState extends State<SubmitVideo> {
 
     return Scaffold(
       appBar: AppBar(title: Text("Submit Video"), centerTitle: true),
-      body: SafeArea(
+      body: Container(
+          padding: EdgeInsets.only(left: width / 25),
+          width: width / 1.3,
           child: Form(
-        key: _formKey,
-        child: ListView(children: <Widget>[
-          TextFormField(
-            inputFormatters: [LengthLimitingTextInputFormatter(30)],
-            keyboardType: TextInputType.text,
-            validator: (val) =>
-                val.isEmpty ? 'Lecture title is required' : null,
-            onSaved: (val) => _lectureTitle = val,
-            decoration: InputDecoration(
-              hintText: 'Enter lecture title',
-              labelText: 'Lecture title',
-            ),
-          ),
-          SizedBox(
-            height: height * 0.1,
-          ),
-          Text("Enter youtube channel key or embed youtube video url beneath"),
-          SizedBox(
-            height: height * 0.1,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.text,
-            onSaved: (val) => _youtubeChannelKey = val,
-            decoration: InputDecoration(
-              hintText: 'Enter Youtube channel key',
-              labelText: 'Youtube channel key',
-            ),
-          ),
-          SizedBox(
-            height: height * 0.1,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.url,
-            onSaved: (val) => _embedVideo = val,
-            decoration: InputDecoration(
-              hintText: 'Enter youtube video url',
-              labelText: 'Youtube video url',
-            ),
-          ),
-          SizedBox(
-            height: height * 0.1,
-          ),
-          RaisedButton(
-              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                side: BorderSide(color: Colors.black87),
+            key: _formKey,
+            child: ListView(children: <Widget>[
+              SizedBox(
+                height: height / 25,
               ),
-              splashColor: Colors.purple,
-              elevation: 10.0,
-              highlightElevation: 30.0,
-              child: const Text(
-                'Submit',
-                style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 20.0,
+              TextFormField(
+                inputFormatters: [LengthLimitingTextInputFormatter(30)],
+                keyboardType: TextInputType.text,
+                validator: (val) =>
+                    val.isEmpty ? 'Lecture title is required' : null,
+                onSaved: (val) => _lectureTitle = val,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(left: 5.0),
+                  icon: Icon(Icons.title),
+                  hintText: 'Enter lecture title',
+                  labelText: 'Lecture Title',
                 ),
               ),
-              color: Colors.yellow[200],
-              textColor: Colors.white,
-              onPressed: () async {
-                if (_formKey.currentState.validate()) {
-                  _formKey.currentState.save();
-                  await FirebaseFirestore.instance.collection("Videos").add({
-                    "isApprovedByAdmin": _isApproved,
-                    "lectureTitle": _lectureTitle,
-                    "videoUrl": _embedVideo,
-                    "youtubeChannelName": _youtubeChannelKey
-                  });
+              SizedBox(
+                height: height * 0.1,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                onSaved: (val) => _youtubeChannelKey = val,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock),
+                  hintText: 'Enter Youtube channel key',
+                  labelText: 'Youtube Channel Key',
+                ),
+              ),
+              SizedBox(
+                height: height * 0.1,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.url,
+                onSaved: (val) => _embedVideo = val,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.link),
+                  hintText: 'Enter youtube video url',
+                  labelText: 'Youtube video url',
+                ),
+              ),
+              SizedBox(
+                height: height * 0.1,
+              ),
+              OutlineButton(
+                  borderSide: BorderSide(color: Color(0xFF3EC3C1), width: 2.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      await FirebaseFirestore.instance
+                          .collection("Videos")
+                          .add({
+                        "isApprovedByAdmin": _isApproved,
+                        "lectureTitle": _lectureTitle,
+                        "videoUrl": _embedVideo,
+                        "youtubeChannelName": _youtubeChannelKey
+                      });
 
-                  _formKey.currentState.reset();
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: const Text("Details Submitted Successfully")));
-                }
-              }),
-        ]),
-      )),
+                      _formKey.currentState.reset();
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content:
+                              const Text("Details Submitted Successfully")));
+                    }
+                  }),
+            ]),
+          )),
     );
   }
 }
