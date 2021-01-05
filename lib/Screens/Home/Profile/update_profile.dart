4e905cc67cab90d77391f1d8d45543625aa8d718
Manthan Gupta <manthangupta109@gmail.com>
2021-01-05 15:27:09 +0530

    Named routes added

--- lib/Screens/Home/Profile/update_profile.dart
+++ lib/Screens/Home/Profile/update_profile.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class UpdateProfile extends StatefulWidget {
+  const UpdateProfile({Key key}) : super(key: key);
   @override
   _UpdateProfileState createState() => _UpdateProfileState();
 }
@@ -71,119 +72,118 @@ class _UpdateProfileState extends State<UpdateProfile> {
 
     return SafeArea(
         child: Container(
-          width: 400.0,
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 25.0, // soften the shadow
-              spreadRadius: 5.0, //extend the shadow
-              offset: Offset(
-                15.0,
-                15.0,
-              ),
-            )
-          ], color: Colors.amber[100], border: Border.all(color: Colors.white)),
-          child: Form(
-            key: _formKey,
-            autovalidateMode: AutovalidateMode.always,
-            child: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              shrinkWrap: true,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (val) =>
-                        val.isEmpty ? 'FullName is required' : null,
-                    onSaved: (val) => _fullName = val,
-                    initialValue: userData.fullName,
-                    decoration: InputDecoration(
-                        hintText: 'Full Name',
-                        icon: Icon(Icons.person),
-                        labelText: "Full Name"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (val) => val.isEmpty ? 'Email is required' : null,
-                    onSaved: (val) => _emailId = val,
-                    initialValue: userData.email,
-                    decoration: InputDecoration(
-                        hintText: 'Email Id',
-                        icon: Icon(Icons.email),
-                        labelText: "Email Id"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (val) => val.isEmpty ? 'DOB is required' : null,
-                    onSaved: (val) => _dob = val,
-                    initialValue: userData.dob,
-                    decoration: InputDecoration(
-                        hintText: 'DOB',
-                        icon: Icon(Icons.date_range),
-                        labelText: "Date of Birth"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (val) =>
-                        val.isEmpty ? 'Qualification is required' : null,
-                    onSaved: (val) => _qualification = val,
-                    initialValue: userData.qualification,
-                    decoration: InputDecoration(
-                        hintText: 'Qualification',
-                        icon: Icon(Icons.grade),
-                        labelText: "Educational Qualification"),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (val) =>
-                        val.isEmpty ? 'University is required' : null,
-                    onSaved: (val) => _university = val,
-                    initialValue: userData.university,
-                    decoration: InputDecoration(
-                        hintText: 'University',
-                        icon: Icon(Icons.account_balance),
-                        labelText: "University"),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(20.0),
-                  child: RaisedButton(
-                    splashColor: Colors.grey,
-                    color: Colors.yellow,
-                    onPressed: () {
-                      _submitForm();
-                    },
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    highlightElevation: 0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text(
-                            'Submit',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.black,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
+      width: 400.0,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.black26,
+          blurRadius: 25.0, // soften the shadow
+          spreadRadius: 5.0, //extend the shadow
+          offset: Offset(
+            15.0,
+            15.0,
           ),
-        ));
+        )
+      ], color: Colors.amber[100], border: Border.all(color: Colors.white)),
+      child: Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.always,
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          shrinkWrap: true,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                validator: (val) => val.isEmpty ? 'FullName is required' : null,
+                onSaved: (val) => _fullName = val,
+                initialValue: userData.fullName,
+                decoration: InputDecoration(
+                    hintText: 'Full Name',
+                    icon: Icon(Icons.person),
+                    labelText: "Full Name"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                validator: (val) => val.isEmpty ? 'Email is required' : null,
+                onSaved: (val) => _emailId = val,
+                initialValue: userData.email,
+                decoration: InputDecoration(
+                    hintText: 'Email Id',
+                    icon: Icon(Icons.email),
+                    labelText: "Email Id"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                validator: (val) => val.isEmpty ? 'DOB is required' : null,
+                onSaved: (val) => _dob = val,
+                initialValue: userData.dob,
+                decoration: InputDecoration(
+                    hintText: 'DOB',
+                    icon: Icon(Icons.date_range),
+                    labelText: "Date of Birth"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                validator: (val) =>
+                    val.isEmpty ? 'Qualification is required' : null,
+                onSaved: (val) => _qualification = val,
+                initialValue: userData.qualification,
+                decoration: InputDecoration(
+                    hintText: 'Qualification',
+                    icon: Icon(Icons.grade),
+                    labelText: "Educational Qualification"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                validator: (val) =>
+                    val.isEmpty ? 'University is required' : null,
+                onSaved: (val) => _university = val,
+                initialValue: userData.university,
+                decoration: InputDecoration(
+                    hintText: 'University',
+                    icon: Icon(Icons.account_balance),
+                    labelText: "University"),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: RaisedButton(
+                splashColor: Colors.grey,
+                color: Colors.yellow,
+                onPressed: () {
+                  _submitForm();
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                highlightElevation: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Submit',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:agriglance/Screens/Home/Profile/profile.dart';
 import 'package:agriglance/Services/authenticate.dart';
+import 'package:agriglance/locator.dart';
+import 'package:agriglance/navigation_service.dart';
+import 'package:agriglance/route_names.dart';
 import 'package:agriglance/services/admob_service.dart';
 import 'package:firebase_admob/firebase_admob.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,14 +10,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
+import 'package:agriglance/router.dart' as router;
 import 'Screens/Home/home.dart';
 import 'Services/authentication_service.dart';
 
 import 'configure_nonweb.dart' if (dart.library.html) 'configure_web.dart';
 
-
 void main() async {
+  setupLocator();
   configureApp();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -36,15 +40,20 @@ class MyApp extends StatelessWidget {
                   context.read<AuthenticationService>().authStateChanges)
         ],
         child: MaterialApp(
+          onGenerateRoute: router.generateRoute,
+          initialRoute: homeViewRoute,
+          navigatorKey: locator<NavigationService>().navigatorKey,
           title: 'Agriglance',
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             backgroundColor: Colors.grey,
             primaryColor: Colors.indigo[900],
           ),
-          home: (kIsWeb)
-              ? Home()
-              : ((FirebaseAuth.instance.currentUser == null) ? Authenticate() : Home()),
+          // home: (kIsWeb)
+          //     ? Home()
+          //     : ((FirebaseAuth.instance.currentUser == null)
+          //         ? Authenticate()
+          //         : Home()),
         ));
   }
 }
